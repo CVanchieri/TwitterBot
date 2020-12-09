3319f67309f2bfeefe2149f05abd5330281340f2
Charles Vanchieri <cvanchieri@charless-mbp.lan>
2020-12-09 13:45:40 -0800

    connected AWS lambda to SQL database

--- createlambdalayer.sh
+++ createlambdalayer.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 if [ "$1" != "" ] || [$# -gt 1]; then
-	echo "Creating layer compatible with python version $1"
-	#docker run --rm -v $(pwd):/foo -w /foo lambci/lambda:build-python3.8  
-	docker run -v "$PWD":/var/task "lambci/lambda:build-python3.8" /bin/sh -c "pip install -r requirements.txt -t libs; exit"
-	pip install -r requirements.txt -t python
-	#docker run -v "$PWD":/var/task "lambci/lambda:build-python3.8" /bin/sh -c "pip install -r requirements.txt -t python/lib/python3.8/site-packages/; exit"
+	echo "Creating layer compatible with python version $1" 
+	#docker run -v "$PWD":/var/task "lambci/lambda:build-python3.8" /bin/sh -c "pip install -r requirements.txt -t libs; exit"
+	#pip install -r requirements.txt -t python
+	docker run -v "$PWD":/var/task "lambci/lambda:build-python3.8" /bin/sh -c "pip install -r requirements.txt -t python/lib/python3.8/site-packages/; exit"
+	#zip -r layer.zip python
+	#docker run -v "$PWD":/var/task "lambci/lambda:build-python$1" /bin/sh -c "pip install -r requirements.txt -t libs; exit"
+	#pip install -r requirements.txt -t python
 	zip -r layer.zip python
-	#docker run -v "$PWD":/var/task "lambci/lambda:build-python$1" /bin/sh -c "pip install -r requirements.txt -t python/lib/python$1/site-packages/; exit"
-	# zip -r layer.zip python > /dev/null
-	# rm -r python
-	# echo "Done creating layer!"
-	# ls -lah layer.zip
+	rm -r python
+	echo "Done creating layer!"
+	#ls -lah layer.zip
 
 else
 	echo "Enter python version as argument - ./createlayer.sh 3.6"
--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,16 +1,17 @@
-# necessary imports 
+### necessary imports ###
 import os
 from pathlib import Path
 from datetime import datetime, timedelta
 import pandas
 from pandas import DataFrame 
 import regex as re
-import tweepy # use to interact with twitter api
+import tweepy
 import psycopg2
 from sqlalchemy import create_engine
 
 ROOT = Path(__file__).resolve().parents[0]
 
+### lambda handler ###
 def lambda_handler(event, context):
     print("---> get Twitter credentials")
     TWITconsumer_key = os.getenv("TWITCONSUMER_KEY")
@@ -24,121 +25,67 @@ def lambda_handler(event, context):
     wait_on_rate_limit_notify=True)
     print("---> Twitter connected")
 
-    tweets_users = {} # store all ids and tweets
-    tweets_hashtags = {} # store all ids and tweets
-    days = 1 # set the # of 'past' days to pull tweets from, end date
-    today = datetime.utcnow()
+    tweets = {} # store all ids and tweets
+    days = 3 # set the # of 'past' days to pull tweets from, end date
+    today = datetime.utcnow() # get todays date
     end_date = today - timedelta(days=days) # <-- set the end date
-    end_str = end_date.strftime('%m/%d/%Y')
-    start = datetime.now()
-
-    ### get tweet_ids for all users in user_names list ###
-    print('--- searching usernames ---')
-    user_names = ['geeksforgeeks', 'TDataScience', 'odsc', 'hackernoon', 
-                  'DataSciNews', 'DataScienceCtrl', 'TensorFlow', 'GoogleAI',
-                  'nordicinst', 'AndrewYNg', 'kdnuggets', 'BernardMarr', 
-                  'KirkDBorne', 'hmason', 'drfeifei', 'NandoDF', 'ilyasut',
-                  'GitHub', 'marcusborba', 'MikeTamir', 'jeremyphoward', 'odsc', 
-                  'pythontrending', 'gvanrossum', 'PythonHub', 'analyticbridge', 
-                  'demishassabis', 'eigensteve', 'MLWhiz', 'JeffDean', 'PyTorch', 
-                  'OpenAI', 'harbrimah', 'rasangarocks', 'ml_india_', 'inm7_isn', 
-                  'mcqubit']
-    for name in user_names: # loop through user names 
-      try:
-        print(f'---> user: {name}')
-        for status in tweepy.Cursor(api.user_timeline,
-                                    screen_name=name, 
-                                    since=end_str,   
-                                    exclude_replies=True,             
-                                    tweet_mode='extended').items(50):
-          if status.full_text is not None:
-            text = status.full_text.lower()
-            strings = ['rt', '@', 'free trial', 'register', 'subscription']
-            if any(string in text for string in strings) is False:
-              key_words = ['artificialintelligence', 'artificial intelligence', 'machinelearning', 'machine learning', 'python',
-              'datascience', 'data science', 'deeplearning', 'deep learning' 'reinforcement learning', 
-              'reinforcementlearning','regression', 'classification', 'neuralnetwork', 'neural network']
-              for word in key_words:
-                if word in text:
-                  id_s = status.id # store the tweet id 
-                  date = status.created_at
-                  name = status.user.name
-                  tweets_users[id_s] = [date, name, text]
-      ### handle errors ###
-      except tweepy.TweepError as e: 
-        print("Tweepy Error: {}".format(e))
- 
-    for key, val in tweets_users.items():
-      val0, val1, val2 = val
-      tags = re.findall("[#]\w+", val2)
-      tweets_users[key] = [val0, val1, val2, tags]
-
-    print('--- users tweets ---')
-    print(f'tweets count: {len(tweets_users)}') 
-    #print(tweets_users)
-    
-    print('--- timer ---')
-    break1 = datetime.now()
-    print("Elapsed time: {0}".format(break1-start))
+    end_str = end_date.strftime('%m/%d/%Y') # set the end date as str
+    start = datetime.now() # set the start date 
 
     ### search hashtags ###
     print('--- searching hashtags ---')
-    tags = ['artificialintelligence', 'machinelearning', 'python', 'datamining', 'datavisualization',
-            'dataalaytics', 'predictivemodeling', 'datascience', 'deeplearning', 'reinforcementlearning', 
-            'neuralnetwork']
-    for tag in tags: # loop through user names 
+    tags = ['datascience', 'machinelearning', 'artificialintelligence']
+    for tag in tags: # loop through hashtags
       try:
         print(f'---> hashtag: {tag}')
         for status in tweepy.Cursor(api.search,q=tag,
                                     since=end_str,   
-                                    exclude_replies=True,             
+                                    exclude_replies=True,    
+                                    lang='en', 
                                     tweet_mode='extended').items(100):
           if status.full_text is not None:
             text = status.full_text.lower()
-            strings = ['rt', '@', 'trial', 'free', 'register', 'subscription']
-            if any(string in text for string in strings) is False:
-                    id_s = status.id # store the tweet id 
-                    date = status.created_at
-                    name = status.user.name
-                    tweets_hashtags[id_s] = [date, name, text]
+
+            id_s = status.id # store the tweet id 
+            date = status.created_at # store the date created 
+            name = status.user.name # store the user name 
+            tweets[id_s] = [date, name, text] # add elements to dict
+            
       ### handle errors ###
       except tweepy.TweepError as e: 
         print("Tweepy Error: {}".format(e))
  
-    for key, val in tweets_hashtags.items():
-      val0, val1, val2 = val
-      tags = re.findall("[#]\w+", val2)
-      tweets_hashtags[key] = [val0, val1, val2, tags]
+    for key, val in tweets.items(): # loop through dictionary key/values
+      val0, val1, val2 = val # unpack all variables 
+      tags = re.findall("[#]\w+", val2) # get all words starting with '#'.
+      tweets[key] = [val0, val1, val2, tags] # add elements to the dictionary
 
     print('--- hashtags tweets ---')
-    print(f'tweets count: {len(tweets_hashtags)}') 
-    #print(tweets_hashtags)
+    print(f'tweets count: {len(tweets)}') # show tweets count
     
     print('--- timer ---')
     break1 = datetime.now()
-    print("Elapsed time: {0}".format(break1-start))
+    print("Elapsed time: {0}".format(break1-start)) # show timer
     
-    ### merge dictionaries ###
-    tweets = {**tweets_users, **tweets_hashtags}
-    print('--- pulled tweets ---')
-    print(f'tweets count: {len(tweets)}') 
-    print(tweets)
     ### create a dataframe for the pulled tweets ###
     print('--- remove any duplicates ---')
     df1 = DataFrame.from_dict(tweets, orient='index', columns=['date', 'name', 'text',  'tags']) # create a dataframe from for the tweets dict
     df1.reset_index(inplace=True) # reset the index 
     df1 = df1.rename(columns = {'index':'id'}) # rename columns 
     df1 = df1.drop_duplicates(subset=['id'], keep='last') # drop duplicates
-    df1[['First','Last']] = df1.text.str.split(n=1, expand=True) 
-    df1 = df1.drop_duplicates(subset=['First'])
-    df1 = df1.drop(columns=['First', 'Last'])
-    df1['retweet'] = 'NO'
+    df1[['First','Last']] = df1.text.str.split(n=1, expand=True)  # split the text column into 2 
+    df1 = df1.drop_duplicates(subset=['First']) # drop duplicates ofr First column
+    df1 = df1.drop(columns=['First', 'Last']) # drop First and Last columns 
+    df1['retweet'] = 'NO' # add a retweet column, set to 'NO'
+    strings = ['rt', '@', 'trial', 'free', 'register', 'subscription'] # list of substrings 
+    df1 = df1[~df1.text.str.contains('|'.join(strings))] # remove any text values that contain a string from strings 
+
     print('--- pulled tweets dataframe ---')
-    print(df1.dtypes)
-    print(df1.shape)
-    print(df1.head())
+    print(df1.dtypes) # show column data types 
+    print(df1.shape) # show dataframe shape 
+    print(df1.head()) # show dataframe
     print('--- pulled tweets text ---')
-    print(df1['text'].values)
+    print(df1['text'].values)  # show the dataframe text values 
 
     print("--- authenticate AWS connection ---")
     AWSdatabase = os.getenv("AWSDATABASE")
@@ -160,67 +107,65 @@ def lambda_handler(event, context):
 
     ### pull the data from SQL ###
     print("--- pulling current AWS database ---")
-    sql_select_Query = "select * from tweets_storage"
+    sql_select_Query = "select * from tweets_storage" # query all of database 
     cur.execute(sql_select_Query)
     records = cur.fetchall()
-    df2 = DataFrame(records)
-    df2.columns = ['id', 'date', 'name', 'text', 'tags', 'retweet']
+    df2 = DataFrame(records)  # set as dataframe 
+    df2.columns = ['id', 'date', 'name', 'text', 'tags', 'retweet'] # label the columns 
     print("---> complete")
     print(" --- current tweets dataframe ---")
-    print(df2.dtypes)
-    print(df2.shape)
-    print(df2.head())
+    print(df2.dtypes) # show the column types 
+    print(df2.shape) # show the dataframe shape 
+    print(df2.head()) # show the dataframe 
 
     print("--- drop current dataframe ---")
-    cur.execute("""DROP TABLE tweets_storage""")
+    cur.execute("""DROP TABLE tweets_storage""") # drop the current table 
     connection.commit()
-    #cur.close()
     print("---> complete")
 
     ### concat the new and old dataframes ###
-    df3 = pandas.concat([df1, df2], axis = 0)
-    df3 = df3.reset_index(drop=True) 
+    df3 = pandas.concat([df1, df2], axis = 0) # merge the old and new dataframes
+    df3 = df3.reset_index(drop=True) # reset the index 
     df3 = df3.drop_duplicates(subset=['id'], keep='last') # drop duplicates
-    df3[['First','Last']] = df3.text.str.split(n=1, expand=True) 
-    df3 = df3.drop_duplicates(subset=['First'])
-    df3 = df3.drop(columns=['First', 'Last'])
+    df3[['First','Last']] = df3.text.str.split(n=1, expand=True) # split the text value into 2 
+    df3 = df3.drop_duplicates(subset=['First']) # drop duplicates 
+    df3 = df3.drop(columns=['First', 'Last']) # remove First and Last columns 
 
-    ### filter tweets that have not been retweeted ### 
-    retweets = df3['retweet'] == 'NO'
+    ## filter tweets that have not been retweeted ### 
+    retweets = df3['retweet'] == 'NO' # create a database of only retweet column values 'NO'
     retweets = df3[retweets]
 
     ### get a random sample from the filtered tweets ###
-    sample = retweets['id'].sample()
+    sample = retweets['id'].sample() # get random sample ID 
     sample = int(sample)
-    api.retweet(sample)
-    df3.loc[df3['id'] == sample,'retweet']='YES'
-    df3 = df3.reset_index(drop=True)
+    api.retweet(sample) # retweet the ID 
+    df3.loc[df3['id'] == sample,'retweet']='YES' # change the 'NO' to 'YES'
+    df3 = df3.reset_index(drop=True) # reset index 
     print('--- tweets available for retweet ---')
-    print(retweets.shape)
-    print(retweets.head(20))
+    print(retweets.shape) # show the dataframe shape 
+    print(retweets.head(20)) # show the dataframe
     print('--- tweet picked for retweet ---')
-    print(sample)
+    print(sample) # show the random id 
 
     ### push the final dataframe to the SQL database ###
     print("--- push to dataframe AWS database ---")
-    engine = create_engine(sql_AWS)
-    df3.to_sql('tweets_storage', con=engine, index=False, if_exists='replace')
+    engine = create_engine(sql_AWS) # create engine
+    df3.to_sql('tweets_storage', con=engine, index=False, if_exists='replace') # push the dataframe to the database
     print("---> complete")
 
     ### final tweets dataframe ###
     print("--- pulling final AWS database ---")
-    sql_select_Query = "select * from tweets_storage"
+    sql_select_Query = "select * from tweets_storage"  # query the whole database 
     cur.execute(sql_select_Query)
     records = cur.fetchall()
-    tweets_database = DataFrame(records)
-    tweets_database.columns = ['id', 'date', 'name', 'text',  'tags', 'retweet']
+    tweets_database = DataFrame(records) # set data as dataframe 
+    tweets_database.columns = ['id', 'date', 'name', 'text',  'tags', 'retweet'] # set the column names 
     cur.close()
 
     print('--- final tweets data frame ---')
-    print(tweets_database.shape)
-    print(tweets_database.head(50))
-    # print(tweets_database.retweet.values())
+    print(tweets_database.shape) # show the dataframe shape 
+    print(tweets_database.head(50)) # show the dataframe 
 
     print('--- timer ---')
     break2 = datetime.now()
-    print("Elapsed time: {0}".format(break2-start))
+    print("Elapsed time: {0}".format(break2-start)) # show timer 
